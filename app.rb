ed02fcb5be9ef6cc4493289d62e6f65692d265ea
KUROKI DAISHI <a1710dk@aiit.ac.jp>
2017-07-19 10:52:45 +0000

    end

--- app.rb
+++ app.rb
@@ -4,3 +4,12 @@ get '/' do
   erb :index
 end
 
+get '/kuroki' do
+  erb :kuroki
+end
+
+post '/kurokiresult' do
+  @network = params[:network]
+  @wariai = params[:wariai]
+  erb :kurokiresult
+end
